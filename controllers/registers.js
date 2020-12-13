068f4e8a316c9581b7babc75a69a99d03866e405
Caio Ribeiro <cacoribeiro@gmail.com>
2020-12-13 11:33:43 -0300

    Status das rotas adicionados

--- controllers/registers.js
+++ controllers/registers.js
@@ -4,18 +4,20 @@ const { Doctors } = require('../models');
 const validationUser = require('../services/validationUser');
 const registerRoute = express.Router();
 
+// ROTA PARA CADASTRAR UM MEDICO
 registerRoute.post('/', async (req, res) => {
   try {
     const newUser = await validationUser({ ...req.body });
     const userStatus = await Doctors.create({ ...req.body });
-    res.status(200).json(userStatus.dataValues);
+    res.status(201).json(userStatus.dataValues);
   } catch (error) {
     error.sql
-      ? res.status(401).json({ message: 'CRM já cadastrado' })
-      : res.status(401).json({ message: error.message });
+      ? res.status(400).json({ message: 'CRM já cadastrado' })
+      : res.status(400).json({ message: error.message });
   }
 });
 
+// ROTA PARA BUSCAR UM MEDICO POR CATEGORIA
 registerRoute.get('/buscar', async (req, res) => {
   try {
     const { search, type } = req.body;
@@ -28,27 +30,44 @@ registerRoute.get('/buscar', async (req, res) => {
     });
     res.status(200).json(findResults);
   } catch (error) {
-    res.status(401).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
+// ROTA PARA EDITAR UM MEDICO
+registerRoute.put('/editar/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await validationUser({ ...req.body });
+    await Doctors.update({ ...req.body }, { where: { id } });
+    const userStatus = await Doctors.findOne({ where: { id } });
+    res.status(200).json(userStatus.dataValues);
+  } catch (error) {
+    error.sql
+      ? res.status(400).json({ message: 'CRM já cadastrado' })
+      : res.status(400).json({ message: error.message });
+  }
+});
+
+// ROTA PARA TRAZER TODOS OS MEDICOS CADASTRADOS NO SISTEMA
 registerRoute.get('/', async (req, res) => {
   try {
     const userStatus = await Doctors.findAll();
     res.status(200).json(userStatus);
   } catch (error) {
-    res.status(401).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
+// ROTA PARA DELETAR UM MEDICO DO SISTEMA
 registerRoute.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const deletedUser = await Doctors.findOne({ where: { id } });
     await Doctors.destroy({ where: { id } });
-    res.status(200).json({ message: `${deletedUser.dataValues.name} foi apagado.` });
+    res.status(204).json({ message: `${deletedUser.dataValues.name} foi apagado.` });
   } catch (error) {
-    res.status(401).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
